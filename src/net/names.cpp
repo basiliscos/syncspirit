fdc7c9ff67298e49b23a619e856f38df2b6ba9fd
Ivan Baidakou <the.dmol@yandex.by>
2021-12-19 21:42:31 +0300

    cleanup

--- src/net/names.cpp
+++ src/net/names.cpp
@@ -5,5 +5,4 @@ using namespace syncspirit::net;
 const char *names::coordinator = "net::coodinator";
 const char *names::resolver = "net::resolver";
 const char *names::http10 = "net::http10";
-const char *names::scan_actor = "fs::scan";
 const char *names::hasher_proxy = "net::hasher_proxy";
--- src/net/names.h
+++ src/net/names.h
@@ -7,7 +7,6 @@ struct names {
     static const char *coordinator;
     static const char *resolver;
     static const char *http10;
-    static const char *scan_actor;
     static const char *hasher_proxy;
 };
 
