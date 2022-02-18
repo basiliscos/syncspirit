3b494a547fdb71630e8e713f802df201e302975e
Ivan Baidakou <the.dmol@yandex.by>
2022-02-18 22:43:09 +0300

    moar tests

--- tests/050-file_iterator.cpp
+++ tests/050-file_iterator.cpp
@@ -6,6 +6,7 @@
 #include "model/diff/modify/share_folder.h"
 #include "model/diff/peer/cluster_update.h"
 #include "model/diff/peer/update_folder.h"
+#include "model/diff/aggregate.h"
 
 using namespace syncspirit;
 using namespace syncspirit::test;
@@ -235,4 +236,95 @@ TEST_CASE("file iterator", "[model]") {
             REQUIRE(!cluster->next_file(peer_device, false));
         }
     }
+}
+
+TEST_CASE("file iterator for 2 folders", "[model]") {
+    auto my_id = device_id_t::from_string("KHQNO2S-5QSILRK-YX4JZZ4-7L77APM-QNVGZJT-EKU7IFI-PNEPBMY-4MXFMQD").value();
+    auto my_device = device_t::create(my_id, "my-device").value();
+    auto peer_id = device_id_t::from_string("VUV42CZ-IQD5A37-RPEBPM4-VVQK6E4-6WSKC7B-PVJQHHD-4PZD44V-ENC6WAZ").value();
+
+    auto peer_device = device_t::create(peer_id, "peer-device").value();
+    auto cluster = cluster_ptr_t(new cluster_t(my_device, 1));
+    cluster->get_devices().put(my_device);
+    cluster->get_devices().put(peer_device);
+
+    auto &folders = cluster->get_folders();
+    db::Folder db_folder1;
+    db_folder1.set_id("1234");
+    db_folder1.set_label("my-label-1");
+    db_folder1.set_path("/my/path");
+
+    db::Folder db_folder2;
+    db_folder2.set_id("5678");
+    db_folder2.set_label("my-label-2");
+    db_folder2.set_path("/my/path");
+
+    auto diffs = diff::aggregate_t::diffs_t{};
+    diffs.push_back(new diff::modify::create_folder_t(db_folder1));
+    diffs.push_back(new diff::modify::create_folder_t(db_folder2));
+    diffs.push_back(new diff::modify::share_folder_t(peer_id.get_sha256(), db_folder1.id()));
+    diffs.push_back(new diff::modify::share_folder_t(peer_id.get_sha256(), db_folder2.id()));
+
+    auto diff = diff::cluster_diff_ptr_t(new diff::aggregate_t(std::move(diffs)));
+    REQUIRE(diff->apply(*cluster));
+    auto folder1 = folders.by_id(db_folder1.id());
+    auto folder2 = folders.by_id(db_folder2.id());
+
+    auto cc = std::make_unique<proto::ClusterConfig>();
+    auto p_folder1 = cc->add_folders();
+    p_folder1->set_id(std::string(folder1->get_id()));
+    p_folder1->set_label(std::string(folder1->get_label()));
+
+    auto p_peer1 = p_folder1->add_devices();
+    p_peer1->set_id(std::string(peer_id.get_sha256()));
+    p_peer1->set_name(std::string(peer_device->get_name()));
+    p_peer1->set_max_sequence(10u);
+    p_peer1->set_index_id(123u);
+
+    auto p_folder2 = cc->add_folders();
+    p_folder2->set_id(std::string(folder2->get_id()));
+    p_folder2->set_label(std::string(folder2->get_label()));
+
+    auto p_peer2 = p_folder2->add_devices();
+    p_peer2->set_id(std::string(peer_id.get_sha256()));
+    p_peer2->set_name(std::string(peer_device->get_name()));
+    p_peer2->set_max_sequence(11u);
+    p_peer2->set_index_id(1234u);
+
+    diff = diff::peer::cluster_update_t::create(*cluster, *peer_device, *cc).value();
+    REQUIRE(diff->apply(*cluster));
+
+    proto::Index idx1;
+    idx1.set_folder(db_folder1.id());
+
+    auto file1 = idx1.add_files();
+    file1->set_name("a.txt");
+    file1->set_sequence(10ul);
+
+    diff = diff::peer::update_folder_t::create(*cluster, *peer_device, idx1).value();
+    REQUIRE(diff->apply(*cluster));
+
+    proto::Index idx2;
+    idx2.set_folder(db_folder2.id());
+
+    auto file2 = idx2.add_files();
+    file2->set_name("b.txt");
+    file2->set_sequence(11ul);
+
+    diff = diff::peer::update_folder_t::create(*cluster, *peer_device, idx2).value();
+    REQUIRE(diff->apply(*cluster));
+
+    auto files = std::unordered_set<std::string>{};
+    auto f = cluster->next_file(peer_device, true);
+    REQUIRE(f);
+    files.emplace(f->get_full_name());
+
+    f = cluster->next_file(peer_device);
+    REQUIRE(f);
+    files.emplace(f->get_full_name());
+
+    REQUIRE(!cluster->next_file(peer_device));
+    CHECK(files.size() == 2);
+    CHECK(files.count("my-label-1/a.txt"));
+    CHECK(files.count("my-label-2/b.txt"));
 }
