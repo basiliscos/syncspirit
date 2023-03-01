41fe6422e3da7c9ea613691c858779348cbc1040
Ivan Baidakou <the.dmol@yandex.by>
2023-03-01 09:57:58 +0300

    rewrite assert

--- src/model/diff/modify/clone_block.cpp
+++ src/model/diff/modify/clone_block.cpp
@@ -20,8 +20,18 @@ clone_block_t::clone_block_t(const file_block_t &file_block) noexcept
         }
     }
     assert(source_file);
-    assert(file_block.file()->get_blocks().at(block_index)->get_hash() ==
-           block_pieces[source_block_index].block()->get_hash());
+    assert(file_block.file()->get_blocks().at(block_index)->get_hash() == source_file->get_folder_info()
+                                                                              ->get_folder()
+                                                                              ->get_cluster()
+                                                                              ->get_folders()
+                                                                              .by_id(folder_id)
+                                                                              ->get_folder_infos()
+                                                                              .by_device_id(device_id)
+                                                                              ->get_file_infos()
+                                                                              .by_name(file_name)
+                                                                              ->get_blocks()
+                                                                              .at(block_index)
+                                                                              ->get_hash());
     auto source_fi = source_file->get_folder_info();
     source_device_id = source_fi->get_device()->device_id().get_sha256();
     source_folder_id = source_fi->get_folder()->get_id();
