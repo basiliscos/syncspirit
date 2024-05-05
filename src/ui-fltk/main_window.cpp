841a51a479ccc34521350bb90eb67ffadc182330
Ivan Baidakou <the.dmol@gmail.com>
2024-05-05 19:28:14 +0300

    WIP

--- src/ui-fltk/main_window.cpp
+++ src/ui-fltk/main_window.cpp
@@ -1,6 +1,6 @@
 #include "main_window.h"
 #include "log_panel.h"
-#include <FL/Fl_Button.H>
+#include <FL/Fl_Box.H>
 
 using namespace syncspirit::fltk;
 
@@ -8,7 +8,9 @@ main_window_t::main_window_t(application_t &application_)
     : parent_t(640, 480, "syncspirit-fltk"), application{application_} {
     int padding = 0;
 
-    log_panel = new log_panel_t(application, padding, padding, w() - padding * 2, h() - padding * 2);
+    auto content = new Fl_Box(padding, padding, w() - padding * 2, (h() / 3 * 2) - padding * 2, "content");
+    log_panel = new log_panel_t(application, padding, padding, w() - padding * 2, (h() / 3) - padding * 2);
+    log_panel->position(0, (h() / 3 * 2));
     end();
 
     resizable(this);
