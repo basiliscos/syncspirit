5c6c871d89965a7bff7cc9163ea844a96a9cbd52
Ivan Baidakou <the.dmol@yandex.by>
2024-05-13 23:53:45 +0300

    Allow rows selection for log_table

--- src/ui-fltk/log_table.h
+++ src/ui-fltk/log_table.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <FL/Fl_Table.H>
+#include <FL/Fl_Table_Row.H>
 #include <deque>
 
 #include "log_sink.h"
@@ -9,8 +9,8 @@ namespace syncspirit::fltk {
 
 struct fltk_sink_t;
 
-struct log_table_t : Fl_Table {
-    using parent_t = Fl_Table;
+struct log_table_t : Fl_Table_Row {
+    using parent_t = Fl_Table_Row;
     using displayed_records_t = std::deque<log_record_t *>;
 
     log_table_t(displayed_records_t &displayed_records, int x, int y, int w, int h);
