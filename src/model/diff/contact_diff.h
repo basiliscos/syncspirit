2ef9312be42b32617e012ccf126c671a0978bebc
Ivan Baidakou <the.dmol@yandex.by>
2024-08-04 17:46:26 +0300

    core, WIP

--- src/model/diff/contact_diff.h
+++ src/model/diff/contact_diff.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-FileCopyrightText: 2019-2022 Ivan Baidakou
+// SPDX-FileCopyrightText: 2019-2024 Ivan Baidakou
 
 #pragma once
 
@@ -9,12 +9,14 @@
 
 namespace syncspirit::model::diff {
 
-struct SYNCSPIRIT_API contact_diff_t : generic_diff_t<tag::contact> {
-    virtual outcome::result<void> visit(contact_visitor_t &, void *custom) const noexcept override;
-};
+struct contact_diff_t;
 
 using contact_diff_ptr_t = boost::intrusive_ptr<contact_diff_t>;
 
-using contact_aggregate_diff_t = aggregate_diff_t<contact_diff_t>;
+struct SYNCSPIRIT_API contact_diff_t : generic_diff_t<tag::contact> {
+    virtual outcome::result<void> visit(contact_visitor_t &, void *custom) const noexcept override;
+
+    contact_diff_ptr_t next;
+};
 
 } // namespace syncspirit::model::diff
