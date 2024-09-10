4f69b904019167a2085fd46ba49ef79116e63a74
Ivan Baidakou <the.dmol@yandex.by>
2024-09-10 15:44:39 +0300

    fltk, peer_folder WIP

--- src/ui-fltk/tree_item/peer_dir_base.cpp
+++ src/ui-fltk/tree_item/peer_dir_base.cpp
@@ -35,30 +35,38 @@ virtual_dir_t *peer_dir_base_t::locate_own_dir(std::string_view name) {
 
 void peer_dir_base_t::add_entry(model::file_info_t &file) {
     bool deleted = file.is_deleted();
+    bool show_deleted = supervisor.get_app_config().fltk_config.display_deleted;
     auto name = file.get_path().filename().string();
     auto name_provider = [this](int index) { return std::string_view(child(index)->label()); };
-    if (file.is_dir()) {
-        within_tree([&]() {
-            auto pos = bisect(name, 0, static_cast<int>(dirs_map.size()) - 1, children(), name_provider);
-            auto t = tree();
-            auto node = new peer_dir_t(supervisor, t, true);
-            node->label(name.data());
-            auto tmp_node = insert(prefs(), "", pos);
-            replace_child(tmp_node, node);
-            t->close(node, 0);
-            file.set_augmentation(node->get_proxy());
-            dirs_map[name] = node;
-            return node;
-        });
-        return;
-    }
-    within_tree([&]() {
-        auto node = new tree_item_t(supervisor, tree(), false);
-        node->label(name.c_str());
-        if (deleted) {
-            node->labelfgcolor(FL_DARK1);
+    auto start_index = int{0};
+    auto end_index = int{0};
+    auto t = tree();
+    auto node = within_tree([&]() -> tree_item_t * {
+        if (file.is_dir()) {
+            end_index = static_cast<int>(dirs_map.size()) - 1;
+            auto dir = new peer_dir_t(supervisor, t, true);
+            dirs_map[name] = dir;
+            return dir;
+        } else {
+            start_index = dirs_map.size();
+            end_index = children() - 1;
+            return new tree_item_t(supervisor, tree(), true);
         }
-        insert_by_label(node, static_cast<int>(dirs_map.size()));
-        return node;
     });
+
+    node->label(name.c_str());
+    file.set_augmentation(node->get_proxy());
+    if (deleted) {
+        node->labelfgcolor(FL_DARK1);
+    }
+    if (!deleted || show_deleted) {
+        auto pos = bisect(name, start_index, end_index, children(), name_provider);
+        auto tmp_node = insert(prefs(), "", pos);
+        replace_child(tmp_node, node);
+        if (file.is_dir()) {
+            t->close(node, 0);
+        }
+    } else {
+        orphaned_items.emplace_back(node);
+    }
 }
