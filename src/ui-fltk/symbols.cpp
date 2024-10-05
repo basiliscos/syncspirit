3cc5427aec6cc0e6a107554a6bcb78d2f31a4d02
Ivan Baidakou <the.dmol@yandex.by>
2024-10-05 22:41:42 +0300

    fltk, fix global buffer overrun

--- src/ui-fltk/symbols.cpp
+++ src/ui-fltk/symbols.cpp
@@ -1,28 +1,28 @@
 #include "symbols.h"
 
-#define UTF8_CAST(X) std::string_view(reinterpret_cast<const char *>(X), sizeof(X))
+#define UTF8_CAST(X) std::string_view(reinterpret_cast<const char *>((X)))
 
 namespace syncspirit::fltk::symbols {
 
-static auto scanning_raw = u8"⟳";
-static auto synchronizing_raw = u8"↓";
-static auto online_raw = u8"⚑";
-static auto offline_raw = u8"⚐";
-static auto connecting_raw = u8"🗲";
-static auto discovering_raw = u8"…";
-static auto deleted_raw = u8"♻";
+static const auto scanning_raw = u8"⟳\0";
+static const auto synchronizing_raw = u8"↓\0";
+static const auto online_raw = u8"⚑\0";
+static const auto offline_raw = u8"⚐\0";
+static const auto connecting_raw = u8"🗲\0";
+static const auto discovering_raw = u8"…\0";
+static const auto deleted_raw = u8"♻\0";
 
 // ♨
 // ♻
 // ⚙
 // ✆
 
-std::string_view scaning = UTF8_CAST(scanning_raw);
-std::string_view syncrhonizing = UTF8_CAST(synchronizing_raw);
-std::string_view online = UTF8_CAST(online_raw);
-std::string_view offline = UTF8_CAST(offline_raw);
-std::string_view connecting = UTF8_CAST(connecting_raw);
-std::string_view discovering = UTF8_CAST(discovering_raw);
-std::string_view deleted = UTF8_CAST(deleted_raw);
+const std::string_view scaning = UTF8_CAST(scanning_raw);
+const std::string_view syncrhonizing = UTF8_CAST(synchronizing_raw);
+const std::string_view online = UTF8_CAST(online_raw);
+const std::string_view offline = UTF8_CAST(offline_raw);
+const std::string_view connecting = UTF8_CAST(connecting_raw);
+const std::string_view discovering = UTF8_CAST(discovering_raw);
+const std::string_view deleted = UTF8_CAST(deleted_raw);
 
 } // namespace syncspirit::fltk::symbols
--- src/ui-fltk/symbols.h
+++ src/ui-fltk/symbols.h
@@ -4,12 +4,12 @@
 
 namespace syncspirit::fltk::symbols {
 
-extern std::string_view online;
-extern std::string_view offline;
-extern std::string_view connecting;
-extern std::string_view discovering;
-extern std::string_view scaning;
-extern std::string_view syncrhonizing;
-extern std::string_view deleted;
+extern const std::string_view online;
+extern const std::string_view offline;
+extern const std::string_view connecting;
+extern const std::string_view discovering;
+extern const std::string_view scaning;
+extern const std::string_view syncrhonizing;
+extern const std::string_view deleted;
 
 } // namespace syncspirit::fltk::symbols
