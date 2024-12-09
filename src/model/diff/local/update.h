a7f00cef13c9bed5f493eff50a2eb1b9f75b426f
Ivan Baidakou <the.dmol@yandex.by>
2024-12-09 21:23:47 +0300

    core, cleanup local_update diff

--- src/model/diff/local/update.h
+++ src/model/diff/local/update.h
@@ -3,19 +3,15 @@
 
 #pragma once
 
-#include <set>
 #include <string_view>
 #include "syncspirit-export.h"
 #include "bep.pb.h"
 #include "model/diff/cluster_diff.h"
 #include "model/misc/sequencer.h"
-#include "utils/string_comparator.hpp"
 
 namespace syncspirit::model::diff::local {
 
 struct SYNCSPIRIT_API update_t final : cluster_diff_t {
-    using blocks_t = std::set<std::string, utils::string_comparator_t>;
-
     update_t(const cluster_t &cluster, sequencer_t &sequencer, std::string_view folder_id,
              proto::FileInfo file) noexcept;
 
