d3de32967d12db15465401dab75737468acf3072
Ivan Baidakou <the.dmol@yandex.by>
2025-03-01 23:16:48 +0300

    use protopuf instead of protobuf (incomplete)

--- tests/053-scan_task.cpp
+++ tests/053-scan_task.cpp
@@ -34,15 +34,17 @@ TEST_CASE("scan_task", "[fs]") {
     cluster->get_devices().put(peer2_device);
 
     auto db_folder = db::Folder();
-    db_folder.set_id("some-id");
-    db_folder.set_label("zzz");
-    db_folder.set_path(root_path.string());
+    db::set_id(db_folder, "some-id");
+    db::set_label(db_folder, "zzz");
+    db::set_path(db_folder, root_path.string());
+
     auto folder = folder_t::create(sequencer->next_uuid(), db_folder).value();
     cluster->get_folders().put(folder);
 
     db::FolderInfo db_folder_info;
-    db_folder_info.set_index_id(1234);
-    db_folder_info.set_max_sequence(3);
+    db::set_index_id(db_folder_info, 1234);
+    db::set_max_sequence(db_folder_info, 3);
+
     auto folder_my = folder_info_t::create(sequencer->next_uuid(), db_folder_info, my_device, folder).value();
     auto folder_peer = folder_info_t::create(sequencer->next_uuid(), db_folder_info, peer_device, folder).value();
     auto folder_peer2 = folder_info_t::create(sequencer->next_uuid(), db_folder_info, peer2_device, folder).value();
@@ -53,23 +55,24 @@ TEST_CASE("scan_task", "[fs]") {
     SECTION("without files"){
 
 #ifndef SYNCSPIRIT_WIN
-        SECTION("no permissions to read dir => err"){bfs::permissions(root_path, bfs::perms::none);
+        SECTION("no permissions to read dir => err"){
+            bfs::permissions(root_path, bfs::perms::none);
+            auto folder_info = folder_info_t::create(sequencer->next_uuid(), db_folder_info, my_device, folder).value();
+            folder->get_folder_infos().put(folder_info);
 
-    auto folder_info = folder_info_t::create(sequencer->next_uuid(), db_folder_info, my_device, folder).value();
-    folder->get_folder_infos().put(folder_info);
+            auto task = scan_task_t(cluster, folder->get_id(), config);
+            auto r = task.advance();
+            CHECK(std::get_if<scan_errors_t>(&r));
 
-    auto task = scan_task_t(cluster, folder->get_id(), config);
-    auto r = task.advance();
-    CHECK(std::get_if<scan_errors_t>(&r));
+            auto errs = std::get_if<scan_errors_t>(&r);
+            REQUIRE(errs->size() == 1);
 
-    auto errs = std::get_if<scan_errors_t>(&r);
-    REQUIRE(errs->size() == 1);
-
-    auto &err = errs->at(0);
-    CHECK(err.ec);
-    CHECK(err.path == root_path);
-}
+            auto &err = errs->at(0);
+            CHECK(err.ec);
+            CHECK(err.path == root_path);
+        }
 #endif
+
 SECTION("no dirs, no files") {
     auto task = scan_task_t(cluster, folder->get_id(), config);
     auto r = task.advance();
@@ -93,8 +96,8 @@ SECTION("some dirs, no files") {
     auto *uf = std::get_if<unknown_file_t>(&r);
     REQUIRE(uf);
     CHECK(uf->path.filename() == "some-dir");
-    CHECK(uf->metadata.size() == 0);
-    CHECK(uf->metadata.type() == proto::FileInfoType::DIRECTORY);
+    CHECK(proto::get_size(uf->metadata) == 0);
+    CHECK(proto::get_type(uf->metadata) == proto::FileInfoType::DIRECTORY);
 
     r = task.advance();
     CHECK(std::get_if<bool>(&r));
@@ -116,8 +119,8 @@ SECTION("no dirs, unknown files") {
     auto *uf = std::get_if<unknown_file_t>(&r);
     REQUIRE(uf);
     CHECK(uf->path.filename() == "some-file");
-    CHECK(uf->metadata.size() == 0);
-    CHECK(uf->metadata.type() == proto::FileInfoType::FILE);
+    CHECK(proto::get_size(uf->metadata) == 0);
+    CHECK(proto::get_type(uf->metadata) == proto::FileInfoType::FILE);
 
     r = task.advance();
     REQUIRE(std::get_if<bool>(&r));
@@ -138,9 +141,9 @@ SECTION("no dirs, symlink to non-existing target") {
     auto *uf = std::get_if<unknown_file_t>(&r);
     REQUIRE(uf);
     CHECK(uf->path.filename() == "symlink");
-    CHECK(uf->metadata.size() == 0);
-    CHECK(uf->metadata.type() == proto::FileInfoType::SYMLINK);
-    CHECK(uf->metadata.symlink_target() == "/some/where");
+    CHECK(proto::get_size(uf->metadata) == 0);
+    CHECK(proto::get_type(uf->metadata) == proto::FileInfoType::SYMLINK);
+    CHECK(proto::get_symlink_target(uf->metadata) == "/some/where");
 
     r = task.advance();
     REQUIRE(std::get_if<bool>(&r));
@@ -152,23 +155,25 @@ SECTION("no dirs, symlink to non-existing target") {
 SECTION("regular files") {
     auto modified = std::int64_t{1642007468};
     auto pr_file = proto::FileInfo{};
-    pr_file.set_name("a.txt");
-    pr_file.set_sequence(4);
-    auto version = pr_file.mutable_version();
-    auto counter = version->add_counters();
-    counter->set_value(1);
-    counter->set_id(peer_device->device_id().get_uint());
+    proto::set_name(pr_file, "a.txt");
+    proto::set_sequence(pr_file, 4);
+
+    auto& v = proto::get_version(pr_file);
+    auto& counter = proto::add_counters(v);
+    proto::set_id(counter, peer_device->device_id().get_uint());
+    proto::set_id(counter, 1);
 
     SECTION("meta is not changed (file)") {
-        pr_file.set_block_size(5);
-        pr_file.set_size(5);
-        pr_file.set_modified_s(modified);
-
         auto path = root_path / "a.txt";
         write_file(path, "12345");
         bfs::last_write_time(path, from_unix(modified));
         auto status = bfs::status(path);
-        pr_file.set_permissions(static_cast<uint32_t>(status.permissions()));
+        auto perms = static_cast<uint32_t>(status.permissions());
+
+        proto::set_block_size(pr_file, 5);
+        proto::set_size(pr_file, 5);
+        proto::set_modified_s(pr_file, modified);
+        proto::set_permissions(pr_file, perms);
 
         auto file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
         REQUIRE(folder_my->add_strict(file));
@@ -189,8 +194,8 @@ SECTION("regular files") {
     }
 
     SECTION("meta is not changed (dir)") {
-        pr_file.set_name("a-dir");
-        pr_file.set_type(proto::FileInfoType::DIRECTORY);
+        proto::set_name(pr_file, "a-dir");
+        proto::set_type(pr_file, proto::FileInfoType::DIRECTORY);
 
         auto dir = root_path / "a-dir";
         bfs::create_directories(dir);
@@ -219,14 +224,9 @@ SECTION("regular files") {
 
     SECTION("meta is not changed (dir + file inside)") {
         auto pr_dir = pr_file;
-        pr_dir.set_name("a-dir-2");
-        pr_dir.set_type(proto::FileInfoType::DIRECTORY);
-        pr_dir.set_sequence(pr_file.sequence() + 1);
-
-        pr_file.set_block_size(5);
-        pr_file.set_size(5);
-        pr_file.set_modified_s(modified);
-        pr_file.set_name("a-dir-2/a.txt");
+        proto::set_name(pr_dir, "a-dir-2");
+        proto::set_type(pr_dir, proto::FileInfoType::DIRECTORY);
+        proto::set_sequence(pr_dir, proto::get_sequence(pr_file) + 1);
 
         auto dir = root_path / "a-dir-2";
         bfs::create_directories(dir);
@@ -235,7 +235,13 @@ SECTION("regular files") {
         write_file(path, "12345");
         bfs::last_write_time(path, from_unix(modified));
         auto status = bfs::status(path);
-        pr_file.set_permissions(static_cast<uint32_t>(status.permissions()));
+        auto perms = static_cast<uint32_t>(status.permissions());
+
+        proto::set_name(pr_file, "a-dir-2/a.txt");
+        proto::set_block_size(pr_file, 5);
+        proto::set_size(pr_file, 5);
+        proto::set_modified_s(pr_file, modified);
+        proto::set_permissions(pr_file, perms);
 
         auto info_file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
         REQUIRE(folder_my->add_strict(info_file));
@@ -268,9 +274,9 @@ SECTION("regular files") {
     }
 
     SECTION("file has been removed") {
-        pr_file.set_block_size(5);
-        pr_file.set_size(5);
-        pr_file.set_modified_s(modified);
+        proto::set_block_size(pr_file, 5);
+        proto::set_size(pr_file, 5);
+        proto::set_modified_s(pr_file, modified);
 
         auto file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
         REQUIRE(folder_my->add_strict(file));
@@ -291,8 +297,8 @@ SECTION("regular files") {
     }
 
     SECTION("dir has been removed") {
-        pr_file.set_name("a-dir");
-        pr_file.set_type(proto::FileInfoType::DIRECTORY);
+        proto::set_name(pr_file, "a-dir");
+        proto::set_type(pr_file, proto::FileInfoType::DIRECTORY);
 
         auto file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
         REQUIRE(folder_my->add_strict(file));
@@ -313,7 +319,7 @@ SECTION("regular files") {
     }
 
     SECTION("removed file does not exist => unchanged meta") {
-        pr_file.set_deleted(true);
+        proto::set_deleted(pr_file, true);
 
         auto file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
         REQUIRE(folder_my->add_strict(file));
@@ -334,8 +340,8 @@ SECTION("regular files") {
     }
 
     SECTION("removed dir does not exist => unchanged meta") {
-        pr_file.set_deleted(true);
-        pr_file.set_type(proto::FileInfoType::DIRECTORY);
+        proto::set_deleted(pr_file, true);
+        proto::set_type(pr_file, proto::FileInfoType::DIRECTORY);
 
         auto file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
         REQUIRE(folder_my->add_strict(file));
@@ -356,7 +362,7 @@ SECTION("regular files") {
     }
 
     SECTION("root dir does not exist & deleted file => unchanged meta") {
-        pr_file.set_deleted(true);
+        proto::set_deleted(pr_file, true);
         folder->set_path(root_path / "zzz");
 
         auto file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
@@ -384,15 +390,16 @@ SECTION("regular files") {
         auto r = scan_result_t{};
 
         SECTION("file size differs") {
-            pr_file.set_block_size(5);
-            pr_file.set_size(6);
-            pr_file.set_modified_s(modified);
-
             auto path = root_path / "a.txt";
             write_file(path, "12345");
             bfs::last_write_time(path, from_unix(modified));
             auto status = bfs::status(path);
-            pr_file.set_permissions(static_cast<uint32_t>(status.permissions()));
+            auto perms = static_cast<uint32_t>(status.permissions());
+
+            proto::set_block_size(pr_file, 5);
+            proto::set_size(pr_file, 6);
+            proto::set_modified_s(pr_file, modified);
+            proto::set_permissions(pr_file, perms);
 
             file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
             REQUIRE(folder_my->add_strict(file));
@@ -407,20 +414,21 @@ SECTION("regular files") {
             REQUIRE(std::get_if<changed_meta_t>(&r));
             auto ref = std::get_if<changed_meta_t>(&r);
             CHECK(ref->file == file);
-            CHECK(ref->metadata.size() == 5);
-            CHECK(ref->metadata.modified_s() == modified);
+            CHECK(proto::get_size(ref->metadata) == 5);
+            CHECK(proto::get_modified_s(ref->metadata) == modified);
         }
 
         SECTION("modification time differs") {
-            pr_file.set_block_size(5);
-            pr_file.set_size(5);
-            pr_file.set_modified_s(modified + 1);
-
             auto path = root_path / "a.txt";
             write_file(path, "12345");
             bfs::last_write_time(path, from_unix(modified));
             auto status = bfs::status(path);
-            pr_file.set_permissions(static_cast<uint32_t>(status.permissions()));
+            auto perms = static_cast<uint32_t>(status.permissions());
+
+            proto::set_block_size(pr_file, 5);
+            proto::set_size(pr_file, 5);
+            proto::set_modified_s(pr_file, modified + 1);
+            proto::set_permissions(pr_file, perms);
 
             file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
             REQUIRE(folder_my->add_strict(file));
@@ -435,15 +443,15 @@ SECTION("regular files") {
             REQUIRE(std::get_if<changed_meta_t>(&r));
             auto ref = std::get_if<changed_meta_t>(&r);
             CHECK(ref->file == file);
-            CHECK(ref->metadata.size() == 5);
-            CHECK(ref->metadata.modified_s() == modified);
+            CHECK(proto::get_size(ref->metadata) == 5);
+            CHECK(proto::get_modified_s(ref->metadata) == modified);
         }
 
         SECTION("permissions differs") {
-            pr_file.set_block_size(5);
-            pr_file.set_size(5);
-            pr_file.set_modified_s(modified);
-            pr_file.set_permissions(static_cast<uint32_t>(-1));
+            proto::set_block_size(pr_file, 5);
+            proto::set_size(pr_file, 5);
+            proto::set_modified_s(pr_file, modified);
+            proto::set_permissions(pr_file, static_cast<uint32_t>(-1));
 
             auto path = root_path / "a.txt";
             write_file(path, "12345");
@@ -470,9 +478,9 @@ SECTION("regular files") {
     }
 
     SECTION("tmp") {
-        pr_file.set_block_size(5);
-        pr_file.set_size(5);
-        pr_file.set_modified_s(modified);
+        proto::set_block_size(pr_file, 5);
+        proto::set_size(pr_file, 5);
+        proto::set_modified_s(pr_file, modified);
 
         auto path = root_path / "a.txt.syncspirit-tmp";
 
@@ -525,10 +533,8 @@ SECTION("regular files") {
             auto file_peer = file_info_t::create(sequencer->next_uuid(), pr_file, folder_peer).value();
             REQUIRE(folder_peer->add_strict(file_peer));
 
-            pr_file.set_size(file_peer->get_size() + 10);
-            auto c2 = version->add_counters();
-            c2->set_id(peer2_device->device_id().get_uint());
-            c2->set_value(2);
+            proto::set_size(pr_file, file_peer->get_size() + 10);
+            proto::add_counters(v, proto::Counter(peer_device->device_id().get_uint(), 2));
 
             auto file_peer2 = file_info_t::create(sequencer->next_uuid(), pr_file, folder_peer2).value();
             REQUIRE(folder_peer2->add_strict(file_peer2));
@@ -567,9 +573,6 @@ SECTION("regular files") {
         }
     }
     SECTION("tmp & non-tmp: both are returned") {
-        pr_file.set_block_size(5);
-        pr_file.set_size(5);
-        pr_file.set_modified_s(modified);
 
         auto path = root_path / "a.txt";
         auto path_tmp = root_path / "a.txt.syncspirit-tmp";
@@ -577,10 +580,15 @@ SECTION("regular files") {
         write_file(path_tmp, "12345");
         bfs::last_write_time(path, from_unix(modified));
         auto status = bfs::status(path);
-        pr_file.set_permissions(static_cast<uint32_t>(status.permissions()));
+        auto perms = static_cast<uint32_t>(status.permissions());
 
+        proto::set_block_size(pr_file, 5);
+        proto::set_size(pr_file, 5);
+        proto::set_modified_s(pr_file, modified);
+        proto::set_permissions(pr_file, perms);
         auto file_my = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
-        counter->set_id(10);
+
+        proto::set_id(counter, 10);
         auto file_peer = file_info_t::create(sequencer->next_uuid(), pr_file, folder_peer).value();
         REQUIRE(folder_my->add_strict(file_my));
         REQUIRE(folder_peer->add_strict(file_peer));
@@ -616,7 +624,7 @@ SECTION("regular files") {
     }
 
     SECTION("cannot read file error") {
-        pr_file.set_name("a.txt");
+        proto::set_name(pr_file, "a.txt");
         auto path = root_path / "a.txt";
         write_file(path, "12345");
 
@@ -641,7 +649,7 @@ SECTION("regular files") {
     }
 
     SECTION("cannot read dir, error") {
-        pr_file.set_name("some/a.txt");
+        proto::set_name(pr_file, "some/a.txt");
         auto path = root_path / "some" / "a.txt";
         auto parent = path.parent_path();
         write_file(path, "12345");
@@ -661,8 +669,8 @@ SECTION("regular files") {
             auto *uf = std::get_if<unknown_file_t>(&r);
             REQUIRE(uf);
             CHECK(uf->path.filename() == "some");
-            CHECK(uf->metadata.size() == 0);
-            CHECK(uf->metadata.type() == proto::FileInfoType::DIRECTORY);
+            CHECK(proto::get_size(uf->metadata) == 0);
+            CHECK(proto::get_type(uf->metadata) == proto::FileInfoType::DIRECTORY);
 
             r = task.advance();
             REQUIRE(std::get_if<scan_errors_t>(&r));
@@ -684,17 +692,17 @@ SECTION("regular files") {
 SECTION("symlink file") {
     auto modified = std::time_t{1642007468};
     auto pr_file = proto::FileInfo{};
-    pr_file.set_name("a.txt");
-    pr_file.set_sequence(4);
-    pr_file.set_type(proto::FileInfoType::SYMLINK);
-    pr_file.set_symlink_target("b.txt");
-    auto version = pr_file.mutable_version();
-    auto counter = version->add_counters();
-    counter->set_id(1);
-    counter->set_value(peer_device->device_id().get_uint());
+
+    proto::set_name(pr_file, "a.txt");
+    proto::set_sequence(pr_file, 4);
+    proto::set_type(pr_file, proto::FileInfoType::SYMLINK);
+    proto::set_symlink_target(pr_file, "b.txt");
+
+    auto& v = proto::get_version(pr_file);
+    proto::add_counters(v, proto::Counter(peer_device->device_id().get_uint(), 1));
 
     SECTION("symlink does not exists") {
-        pr_file.set_modified_s(modified);
+        proto::set_modified_s(pr_file, modified);
 
         auto path = root_path / "a.txt";
         auto target = bfs::path("b.txt");
@@ -719,8 +727,8 @@ SECTION("symlink file") {
     }
 
     SECTION("symlink does exists") {
-        pr_file.set_modified_s(modified);
-        pr_file.set_symlink_target("b");
+        proto::set_modified_s(pr_file, modified);
+        proto::set_symlink_target(pr_file, "b");
 
         auto path = root_path / "a.txt";
         auto target = bfs::path("b");
@@ -728,10 +736,10 @@ SECTION("symlink file") {
         bfs::create_directories(root_path / target);
 
         auto pr_dir = proto::FileInfo{};
-        pr_dir.set_name("b");
-        pr_dir.set_sequence(pr_file.sequence() + 1);
-        pr_dir.set_type(proto::FileInfoType::DIRECTORY);
-        *pr_dir.mutable_version() = pr_file.version();
+        proto::set_name(pr_dir, "b");
+        proto::set_sequence(pr_dir, proto::get_sequence(pr_file) + 1);
+        proto::set_type(pr_dir, proto::FileInfoType::DIRECTORY);
+        proto::set_version(pr_dir, v);
 
         auto file = file_info_t::create(sequencer->next_uuid(), pr_file, folder_my).value();
         auto dir = file_info_t::create(sequencer->next_uuid(), pr_dir, folder_my).value();
@@ -763,8 +771,8 @@ SECTION("symlink file") {
     }
 
     SECTION("symlink to root") {
-        pr_file.set_modified_s(modified);
-        pr_file.set_symlink_target("/");
+        proto::set_modified_s(pr_file, modified);
+        proto::set_symlink_target(pr_file, "/");
 
         auto path = root_path / "a.txt";
         auto target = bfs::path("/");
@@ -789,7 +797,7 @@ SECTION("symlink file") {
     }
 
     SECTION("symlink points to something different") {
-        pr_file.set_modified_s(modified);
+        proto::set_modified_s(pr_file, modified);
 
         auto path = root_path / "a.txt";
         auto target = bfs::path("c.txt");
