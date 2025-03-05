b38e063c80f3ad9c35fb9e0e72494b28fb50e5ae
Ivan Baidakou <the.dmol@yandex.by>
2025-03-05 14:40:11 +0300

    use protopuf instead of protobuf (incomplete)

--- src/ui-daemon/command/add_folder.cpp
+++ src/ui-daemon/command/add_folder.cpp
@@ -56,18 +56,18 @@ outcome::result<command_ptr_t> add_folder_t::construct(std::string_view in) noex
     }
 
     db::Folder f;
-    f.path(path);
-    f.id(id);
-    f.label(label);
-    f.read_only(false);
-    f.ignore_permissions(true);
-    f.ignore_delete(false);
-    f.disable_temp_indexes(true);
-    f.paused(false);
-    f.scheduled(false);
-    f.folder_type(db::FolderType::send_and_receive);
-    f.pull_order(db::PullOrder::random);
-    f.rescan_interval(3600);
+    db::set_path(f, path);
+    db::set_id(f, id);
+    db::set_label(f, label);
+    db::set_read_only(f, false);
+    db::set_ignore_permissions(f, false);
+    db::set_ignore_delete(f, false);
+    db::set_disable_temp_indexes(f, false);
+    db::set_paused(f, false);
+    db::set_scheduled(f, false);
+    db::set_folder_type(f, db::FolderType::send_and_receive);
+    db::set_pull_order(f, db::PullOrder::random);
+    db::set_rescan_interval(f, 3600);
     return command_ptr_t(new add_folder_t(std::move(f)));
 }
 
@@ -78,16 +78,16 @@ bool add_folder_t::execute(governor_actor_t &actor) noexcept {
 
     for (auto it : cluster->get_folders()) {
         auto f = it.item;
-        if ((f->get_id() == folder.id())) {
+        if ((f->get_id() == db::get_id(folder))) {
             log->warn("{}, folder with id = {} is already present is the cluster", actor.get_identity(), f->get_id());
             return false;
         }
-        if (f->get_label() == folder.label()) {
+        if (f->get_label() == db::get_label(folder)) {
             log->warn("{}, folder with label = {} is already present is the cluster", actor.get_identity(),
                       f->get_label());
             return false;
         }
-        if (f->get_path().string() == folder.path()) {
+        if (f->get_path().string() == db::get_path(folder)) {
             log->warn("{}, folder with path = {} is already present is the cluster", actor.get_identity(),
                       f->get_path());
             return false;
@@ -97,12 +97,12 @@ bool add_folder_t::execute(governor_actor_t &actor) noexcept {
     auto opt = modify::upsert_folder_t::create(*cluster, *actor.sequencer, folder, 0);
     if (opt.has_error()) {
         auto message = opt.assume_error().message();
-        log->warn("{}, cannot create folder '{}' on '{}': {}", actor.get_identity(), folder.label(), folder.path(),
+        log->warn("{}, cannot create folder '{}' on '{}': {}", actor.get_identity(), db::get_label(folder), db::get_path(folder),
                   message);
         return false;
     }
 
-    log->debug("{}, going to add folder '{}' on '{}'", actor.get_identity(), folder.label(), folder.path());
+    log->debug("{}, going to add folder '{}' on '{}'", actor.get_identity(), db::get_label(folder), db::get_path(folder));
 
     actor.send_command(std::move(opt.value()), *this);
     return true;
