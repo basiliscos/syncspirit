a2e910214b84d1833a7707717a7e0b7f5bd42bd7
Ivan Baidakou <the.dmol@yandex.by>
2025-03-11 20:16:56 +0300

    [core] add db migration: 1 -> 2

--- src/db/utils.h
+++ src/db/utils.h
@@ -20,6 +20,7 @@ using pair_t = model::diff::load::pair_t;
 using container_t = model::diff::load::container_t;
 
 SYNCSPIRIT_API outcome::result<std::uint32_t> get_version(transaction_t &txn) noexcept;
+SYNCSPIRIT_API outcome::result<void> save_version(std::uint32_t v, transaction_t &txn) noexcept;
 SYNCSPIRIT_API outcome::result<void> migrate(std::uint32_t from, model::device_ptr_t device,
                                              transaction_t &txn) noexcept;
 
