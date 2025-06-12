8899cad9601a4761db9ee37064a1bbd059a7ec05
Ivan Baidakou <the.dmol@yandex.by>
2025-06-12 11:09:45 +0300

    fltk: fix use-after-free

--- src/ui-fltk/presence_item.cpp
+++ src/ui-fltk/presence_item.cpp
@@ -78,6 +78,9 @@ presence_item_t::~presence_item_t() {
     while (children() > 0) {
         safe_detach(0);
     }
+    if (presence) {
+        presence->get_augmentation().detach();
+    }
 }
 
 void presence_item_t::on_open() {
