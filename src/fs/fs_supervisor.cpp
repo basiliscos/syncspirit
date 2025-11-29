d43c2bb5d9a13fae452ec614ae742214228005ac
Ivan Baidakou <the.dmol@yandex.by>
2025-11-29 20:51:03 +0300

    core: local_keeper, WIP

--- src/fs/fs_supervisor.cpp
+++ src/fs/fs_supervisor.cpp
@@ -6,12 +6,6 @@
 
 using namespace syncspirit::fs;
 
-namespace {
-namespace resource {
-r::plugin::resource_id_t interrupt = 0;
-} // namespace resource
-} // namespace
-
 fs_supervisor_t::fs_supervisor_t(config_t &cfg)
     : parent_t(cfg), fs_config{cfg.fs_config}, hasher_threads{cfg.hasher_threads} {
     rw_cache.reset(new file_cache_t(fs_config.mru_size));
