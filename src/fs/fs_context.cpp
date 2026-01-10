b82f253e0136a1cda75df6e38e94b28bae4197ed
Ivan Baidakou <the.dmol@yandex.by>
2026-01-10 23:27:01 +0300

    core, linux, fs: introduce epoll

--- src/fs/fs_context.cpp
+++ src/fs/fs_context.cpp
@@ -29,6 +29,56 @@ template <> auto &supervisor_t::access<to::queue>() noexcept { return queue; }
 template <> auto &supervisor_t::access<to::poll_duration>() noexcept { return poll_duration; }
 template <> auto &supervisor_t::access<to::inbound_queue>() noexcept { return inbound_queue; }
 
+#ifdef SYNCSPIRIT_WATCHER_INOTIFY
+fs_context_t::io_context_t::io_context_t(fs_context_t::io_callback_t callback_, void *data_, int index_)
+    : cb(std::move(callback_)), data{data_}, index{index_} {}
+#endif
+
+#ifdef SYNCSPIRIT_WATCHER_INOTIFY
+fs_context_t::io_guard_t::io_guard_t() : fd{0} {};
+fs_context_t::io_guard_t::io_guard_t(fs_context_t *ctx_, int fd_) : ctx{ctx_}, fd{fd_} {};
+fs_context_t::io_guard_t::io_guard_t(io_guard_t &&other) : fd{0} {
+    std::swap(ctx, other.ctx);
+    std::swap(fd, other.fd);
+}
+fs_context_t::io_guard_t::~io_guard_t() {
+    if (fd && ctx) {
+        if (epoll_ctl(ctx->epoll_fd, EPOLL_CTL_DEL, fd, nullptr) == -1) {
+            auto log = utils::get_logger("fs");
+            LOG_CRITICAL(log, "cannot epoll_ctl(del): {}", strerror(errno));
+        }
+        auto it = ctx->io_callbacks.find(fd);
+        if (it != ctx->io_callbacks.end()) {
+            ctx->io_callbacks.erase(it);
+        }
+    }
+}
+fs_context_t::io_guard_t &fs_context_t::io_guard_t::operator=(io_guard_t &&other) {
+    std::swap(ctx, other.ctx);
+    std::swap(fd, other.fd);
+    return *this;
+}
+fs_context_t::io_guard_t::operator bool() const { return fd; }
+#endif
+
+#ifdef SYNCSPIRIT_WATCHER_INOTIFY
+static void async_cb(int fd, void *data) {
+    auto ctx = reinterpret_cast<fs_context_t *>(data);
+    char dummy[4];
+    bool do_read = true;
+    while (do_read) {
+        auto r = ::read(fd, &dummy, sizeof(dummy));
+        do_read = r > 0;
+        if (r < 0) {
+            if (errno != EAGAIN) {
+                LOG_WARN(ctx->log, "read() failed: {}", strerror(errno));
+            }
+        }
+    }
+    ctx->async_flag.store(false, std::memory_order_release);
+}
+#endif
+
 fs_context_t::fs_context_t() noexcept
 #ifdef SYNCSPIRIT_WATCHER_INOTIFY
     : async_flag{false}
@@ -38,19 +88,35 @@ fs_context_t::fs_context_t() noexcept
 {
     log = utils::get_logger("fs");
 #ifdef SYNCSPIRIT_WATCHER_INOTIFY
-    async_pipes[0] = async_pipes[1] = -1;
-    int fds[2];
-    auto r = pipe(fds);
-    if (r == 0) {
-        for (int i = 0; i < 2; ++i) {
-            auto fd = fds[i];
-            fcntl(fd, F_SETFD, FD_CLOEXEC);
-            fcntl(fd, F_SETFD, O_NONBLOCK);
-            async_pipes[i] = fd;
-        }
+    async_pipes[0] = async_pipes[1] = epoll_fd = -1;
+
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0) {
+        LOG_CRITICAL(log, "cannot epoll_create1(): {}", strerror(errno));
     } else {
-        LOG_CRITICAL(log, "cannot pipe(): {}", strerror(errno));
+        int fds[2];
+        auto r = pipe(fds);
+        if (r == 0) {
+            for (int i = 0; i < 2; ++i) {
+                auto fd = fds[i];
+                int flags = fcntl(fd, F_GETFL, 0);
+                if (flags == -1) {
+                    LOG_CRITICAL(log, "cannot fcntl(): {}", strerror(errno));
+                } else {
+                    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+                        LOG_CRITICAL(log, "cannot fcntl(): {}", strerror(errno));
+                    } else {
+                        async_pipes[i] = fd;
+                    }
+                }
+            }
+
+            async_guard = register_callback(async_pipes[0], async_cb, this);
+        } else {
+            LOG_CRITICAL(log, "cannot pipe(): {}", strerror(errno));
+        }
     }
+
 #elif SYNCSPIRIT_WATCHER_WIN32
     async_event = ::CreateEvent(nullptr, false, false, nullptr);
     if (!async_event) {
@@ -61,8 +127,14 @@ fs_context_t::fs_context_t() noexcept
 
 fs_context_t::~fs_context_t() {
 #if SYNCSPIRIT_WATCHER_INOTIFY
+    async_guard = {};
     for (int i = 0; i < 2; ++i) {
-        close(async_pipes[i]);
+        if (async_pipes[i] >= 0) {
+            close(async_pipes[i]);
+        }
+    }
+    if (epoll_fd >= 0) {
+        close(epoll_fd);
     }
 #elif SYNCSPIRIT_WATCHER_WIN32
     if (async_event) {
@@ -113,49 +185,58 @@ void fs_context_t::run() noexcept {
     root_sup.do_process();
 }
 
-void fs_context_t::wait_next_event() noexcept {
+#if SYNCSPIRIT_WATCHER_INOTIFY
+auto fs_context_t::register_callback(int fd, io_callback_t callback, void *data) -> io_guard_t {
+    auto &event = events.emplace_back(epoll_event{});
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event) == -1) {
+        LOG_ERROR(log, "cannot epoll_ctl(): {}", strerror(errno));
+        return {};
+    } else {
+        auto index = static_cast<int>(events.size());
+        io_callbacks.emplace(fd, io_context_t(callback, data, index));
+        return io_guard_t(this, fd);
+    }
+}
+#endif
+
+bool fs_context_t::wait_next_event() noexcept {
     using namespace std::chrono;
     using namespace std::chrono_literals;
 
 #if SYNCSPIRIT_WATCHER_INOTIFY
-    struct timeval timeout;
+    // auto timeout = int{60'000};
+    auto timeout = int{1'000};
+    // auto timeout = int{5'000};
 
     if (!timer_nodes.empty()) {
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 0;
         auto &next = timer_nodes.front().deadline;
         auto delta = next - clock_t::now();
-        auto secs_dur = duration_cast<seconds>(delta);
-        auto secs = secs_dur.count();
-        if (secs >= 0) {
-            timeout.tv_sec = secs;
-            auto u_dur = duration_cast<microseconds>(delta);
-            auto u = u_dur.count() - secs * 1'000'000;
-            if (u > 0) {
-                timeout.tv_usec = u;
+        auto milli_dur = duration_cast<milliseconds>(delta);
+        auto milli = milli_dur.count() + 1; // to round up
+        timeout = std::max(0, static_cast<int>(milli));
+    }
+
+    for (auto &event : events) {
+        event.events = EPOLLIN;
+    }
+
+    auto r = ::epoll_wait(epoll_fd, events.data(), static_cast<int>(events.size()), timeout);
+    bool timed_out = r == 0;
+    // LOG_ERROR(log, "zzz r = {}, timeout = {}", r, timeout);
+    if (r == -1) {
+        LOG_WARN(log, "epoll_wait() failed: {}", strerror(errno));
+    } else if (r) {
+        for (auto &event : events) {
+            if (event.events & EPOLLIN) {
+                auto fd = event.data.fd;
+                auto &ctx = io_callbacks.at(fd);
+                ctx.cb(fd, ctx.data);
             }
         }
-    } else {
-        timeout.tv_usec = 0;
-        timeout.tv_sec = 60;
     }
-
-    fd_set read_fds;
-    auto max_fd = async_pipes[0];
-    FD_ZERO(&read_fds);
-    FD_SET(max_fd, &read_fds);
-
-    auto r = ::select(max_fd + 1, &read_fds, NULL, NULL, &timeout);
-    if (r == -1) {
-        LOG_WARN(log, "select() failed: {}", strerror(errno));
-    } else if (r) {
-        char dummy[4];
-        r = ::read(max_fd, &dummy, sizeof(dummy));
-        if (r < 0) {
-            LOG_WARN(log, "read() failed: {}", strerror(errno));
-        }
-        async_flag.store(true, std::memory_order_release);
-    }
+    return timed_out;
 #elif SYNCSPIRIT_WATCHER_WIN32
     auto wait_millisecs = std::uint32_t{60'000};
     if (!timer_nodes.empty()) {
--- src/fs/fs_context.hpp
+++ src/fs/fs_context.hpp
@@ -10,6 +10,8 @@
 
 #if SYNCSPIRIT_WATCHER_INOTIFY
 #include <atomic>
+#include <unordered_map>
+#include <sys/epoll.h>
 #elif SYNCSPIRIT_WATCHER_WIN32
 #include <windows.h>
 #endif
@@ -26,11 +28,38 @@ struct SYNCSPIRIT_API fs_context_t : rth::system_context_thread_t {
     ~fs_context_t();
 
     void run() noexcept override;
-    void wait_next_event() noexcept;
+    bool wait_next_event() noexcept;
 
 #if SYNCSPIRIT_WATCHER_INOTIFY
+    using io_callback_t = void (*)(int, void *);
+    struct io_context_t {
+        io_context_t(fs_context_t::io_callback_t callback_, void *data_, int index_);
+        io_callback_t cb;
+        void *data;
+        int index;
+    };
+    using io_callbacks_map_t = std::unordered_map<int, io_context_t>;
+    using events_t = std::vector<epoll_event>;
+    struct io_guard_t {
+        io_guard_t();
+        io_guard_t(fs_context_t *, int fd);
+        io_guard_t(io_guard_t &&);
+        ~io_guard_t();
+
+        io_guard_t &operator=(io_guard_t &&);
+        operator bool() const;
+        fs_context_t *ctx;
+        int fd;
+    };
+
+    io_guard_t register_callback(int, io_callback_t, void *data);
+
+    int epoll_fd;
     int async_pipes[2];
     std::atomic_bool async_flag;
+    io_callbacks_map_t io_callbacks;
+    events_t events;
+    io_guard_t async_guard;
 #elif SYNCSPIRIT_WATCHER_WIN32
     HANDLE async_event;
 #endif
