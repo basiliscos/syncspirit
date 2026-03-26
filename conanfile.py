69e7501794c338618f6b9eef73beab94bfe0d386
Ivan Baidakou <the.dmol@yandex.by>
2026-03-26 20:02:45 +0300

    conan: optionalize fltk-frontend build

--- conanfile.py
+++ conanfile.py
@@ -5,19 +5,28 @@ class SyncspiritRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
+        "frontend_fltk": [True, False],
+    }
+    default_options = {
+        "shared": True,
+        "frontend_fltk": True,
     }
 
     def requirements(self):
-        self.requires("freetype/2.12.1", headers=True, libs=True, transitive_libs=True, force=True)
-        self.requires("fltk/1.3.9", libs=True, transitive_libs=True, force=True)
+        rotor_options = {"enable_asio": True, "enable_thread": True}
 
-        self.requires("libqrencode/4.1.1")
+        if self.options.get_safe("frontend_fltk", False):
+            self.requires("freetype/2.12.1", headers=True, libs=True, transitive_libs=True, force=True)
+            self.requires("fltk/1.3.9", libs=True, transitive_libs=True, force=True)
+            self.requires("libqrencode/4.1.1")
+            rotor_options["enable_fltk"] = True
+
+        self.requires("rotor/0.40", options=rotor_options)
         self.requires("lz4/1.10.0")
         self.requires("nlohmann_json/3.12.0")
         self.requires("openssl/3.5.2")
         self.requires("protopuf/3.0.0")
         self.requires("pugixml/1.15")
-        self.requires("rotor/0.40")
         self.requires("spdlog/1.15.3")
         self.requires("tomlplusplus/3.4.0")
         self.requires("zlib/1.3.1")
@@ -32,6 +41,7 @@ class SyncspiritRecipe(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.variables["SYNCSPIRIT_FRONTEND_FLTK"] = self.options.get_safe("frontend_fltk", False)
         tc.generate()
         tc = CMakeDeps(self)
         tc.generate()
